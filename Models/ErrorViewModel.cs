1557725ecf34da3c21962f0fc1243ae9689ade6f
ChrisNrgaard <119853792+ChrisNrgaard@users.noreply.github.com>
2023-12-06 21:32:27 +0100

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PrøveEksamenMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
